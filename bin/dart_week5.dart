7f0c64c7364e6ac46be000927e61a441600162d7
hitori1253 <jont2514@gmail.com>
2025-11-21 11:13:41 +0700

    hitori

--- bin/dart_week5.dart
+++ bin/dart_week5.dart
@@ -1,6 +1,12 @@
-import 'package:dart_week5/function_call.dart';
+//import 'package:dart_week5/dart_week5.dart';
+//import 'package:dart_week5/return.dart';
+//import 'package:dart_week5/function_call.dart';
+
+import 'package:dart_week5/opitonl_position.dart' show callPrint;
 
 void main(){
-  printName();
-  del142();
+  //printName();
+  //calfunctionc();
+  //del142();
+  callPrint();
 }
---
+++ lib/opitonl_position.dart
@@ -0,0 +1,7 @@
+void printIfo(String name,String gender,[String? title]){
+  print("ดีจ้า $title$name,เพศอะไรหรอ $gender");
+}
+
+void callPrint(){
+  printIfo("hitori" ,"ไม่บอกแบร่!!",);
+}
